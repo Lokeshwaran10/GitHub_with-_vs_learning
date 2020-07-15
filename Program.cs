c7f8e2106bfd3358076c81e19ac5d8d7017deee8
Lokesh <officiallokesh96@gmail.com>
2020-07-15 12:12:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHub_with__vs_learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
